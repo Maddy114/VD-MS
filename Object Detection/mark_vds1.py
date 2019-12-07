fffb3f1fcb7fcac82c0a7557732ec4b0bb5ede03
Mahesh Sethi <32360466+Maddy114@users.noreply.github.com>
2019-12-07 21:47:59 +0530

    Add files via upload

--- Object Detection/mark_vds1.py
+++ Object Detection/mark_vds1.py
@@ -1,58 +1,86 @@
 import cv2
+import time
 import numpy as np
+
 net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+
+#net = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny.cfg")
 classes = []
-class_ids = []
-boxes = []
-confidences = []
 with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
-img = cv2.imread("room_ser.jpg")
-img = cv2.resize(img, None, fx=0.4, fy=0.4)
-height,width,channels = img.shape
-blob = cv2.dnn.blobFromImage(img, 0.00392,(416 ,416), (0,0,0), True, crop = False)
 
-for b in blob:
-    for n,img_blob in enumerate(b):
-        cv2.imshow(str(n),img_blob)
+#img = cv2.imread("room_ser.jpg")
+#img = cv2.resize(img, None, fx=0.4, fy=0.4)
 
-net.setInput(blob)
-outs = net.forward(output_layers)
+cap = cv2.VideoCapture(0)
 
-for out in outs :
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.5:
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
+starting_time = time.time()
+frame_id = 0
 
-            #cv2.circle(img, (center_x, center_y),10,(0,255,0),2)
-            x = int(center_x - w / 2 )
-            y = int(center_y - h / 2 )
-
-            boxes.append([x,y,w,h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
-
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5,0.4)
-print(indexes)
-#number_objects_detected = len(boxes)
 font = cv2.FONT_HERSHEY_PLAIN
-for i in range(len(boxes)):
-    if i in indexes:
-        x,y,w,h = boxes[i]
-        label = str(classes[class_ids[i]])
-        color = colors[i]
-        cv2.rectangle(img,(x,y), (x+w, y+h),color,2)
-        cv2.putText(img, label, (x,y + 30), font, 3, color,3)
+while True:
+    _,frame = cap.read()
+    frame_id += 1
+    height,width,channels = frame.shape
+    #changing size of image to 320 from 416
+    blob = cv2.dnn.blobFromImage(frame, 0.00392,(320 ,320), (0,0,0), True, crop = False)
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
+    for b in blob:
+        for n,frame_blob in enumerate(b):
+            cv2.imshow(str(n),frame_blob)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    class_ids = []
+    boxes = []
+    confidences = []    
+    for out in outs :
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            #if confidence > 0.5:
+            # if confidence > 0.2:
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+ 
+                #cv2.circle(frame, (center_x, center_y),10,(0,255,0),2)
+                x = int(center_x - w / 2 )
+                y = int(center_y - h / 2 )
+
+                boxes.append([x,y,w,h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+                #prajesht@gmail.com
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5,0.4)
+    #print(indexes)
+    #number_objects_detected = len(boxes)
+    
+    for i in range(len(boxes)):
+        if i in indexes:
+            x,y,w,h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = confidences[i]
+            color = colors[class_ids[i]]
+            cv2.rectangle(frame,(x,y), (x+w, y+h),color,2)
+            cv2.putText(frame, label + " " + str(round(confidence,2)), (x,y + 30), font, 3, color,3)
+            
+
+    elapsed_time = time.time() - starting_time
+    fps = frame_id / elapsed_time
+    cv2.putText(frame,"FPS: " + str(round(fps,2)),(10,30), font,3 , (0, 0,0), 2)
+    cv2.imshow("Image", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cv2.release()  
 cv2.destroyAllWindows()
